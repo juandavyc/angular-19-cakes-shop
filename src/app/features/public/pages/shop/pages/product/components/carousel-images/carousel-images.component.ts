3524111b8aee0fcc0e063f3f5417185f1130869a
juandavyc <davidy0090011@gmail.com>
2025-06-01 15:49:45 -0500

    fix swipe

--- src/app/features/public/pages/shop/pages/product/components/carousel-images/carousel-images.component.ts
+++ src/app/features/public/pages/shop/pages/product/components/carousel-images/carousel-images.component.ts
@@ -1,78 +1,54 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, inject, input, output, viewChild } from '@angular/core';
+import { afterNextRender, AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, inject, input, CUSTOM_ELEMENTS_SCHEMA , viewChild } from '@angular/core';
 import { ProductImage } from '../../interfaces/product-response.interface';
-import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
-
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import { PlatformIdService } from '@shared/service/platform-id.service';
+
+import { SwiperContainer } from 'swiper/element';
+import { SwiperOptions } from 'swiper/types';
+
+
 
 
 @Component({
   selector: 'carousel-images',
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './carousel-images.component.html',
   styleUrl: './carousel-images.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CarouselImagesComponent implements AfterViewInit {
+export class CarouselImagesComponent {
 
 
   public cover = input.required<string>();
   public images = input.required<ProductImage[]>();
 
-  private swiperDiv = viewChild.required<ElementRef<HTMLDivElement>>('swiperDiv');
+  private swiperRef = viewChild.required<ElementRef<SwiperContainer>>('swiperRef');
 
-  private platformIdService = inject(PlatformIdService)
-
-  // const swiper = new Swiper('.swiper', {
-  //   // configure Swiper to use modules
-  //   modules: [Navigation, Pagination],
-  //   ...
-  // });
+  constructor() {
+    afterNextRender(() => {
+      this.initSwiper();
+    });
+  }
 
 
+  private initSwiper() {
+    const swiperOptions: SwiperOptions = {
+      slidesPerView: 1,  // Muestra solo 1 slide a la vez
+      spaceBetween: 30,
+      loop: true,        // Permite navegación infinita
+      centeredSlides: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        clickable: true,
+      },
+    };
 
 
-  ngAfterViewInit(): void {
-
-
-    const element = this.swiperDiv().nativeElement;
-    if (!element) return;
-    if (this.platformIdService.isBrowser()) {
-      const swiper = new Swiper(element, {
-        direction: 'horizontal',
-        loop: true,
-        modules: [Navigation, Pagination],
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
-        breakpoints: {
-          640: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 1,
-            spaceBetween: 30,
-          },
-          1024: {
-            slidesPerView: 1,
-            spaceBetween: 40,
-          },
-        }
-      });
-    }
-
-
+    Object.assign(this.swiperRef().nativeElement, swiperOptions);
+    this.swiperRef().nativeElement.initialize();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { enableProdMode } from '@angular/core';
-
+import { register } from 'swiper/element/bundle';
 // enableProdMode()
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
+register();
