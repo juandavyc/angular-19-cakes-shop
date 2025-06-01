93f24ad136e793ef7508b20a1d1a2e2cbbc3e893
juandavyc <davidy0090011@gmail.com>
2025-06-01 15:27:35 -0500

    fix swipe no displaying images correctly

--- src/app/features/public/pages/shop/pages/product/components/carousel-images/carousel-images.component.ts
+++ src/app/features/public/pages/shop/pages/product/components/carousel-images/carousel-images.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, input, output, viewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, inject, input, output, viewChild } from '@angular/core';
 import { ProductImage } from '../../interfaces/product-response.interface';
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
@@ -6,6 +6,7 @@ import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import { PlatformIdService } from '@shared/service/platform-id.service';
 
 
 @Component({
@@ -23,6 +24,8 @@ export class CarouselImagesComponent implements AfterViewInit {
 
   private swiperDiv = viewChild.required<ElementRef<HTMLDivElement>>('swiperDiv');
 
+  private platformIdService = inject(PlatformIdService)
+
   // const swiper = new Swiper('.swiper', {
   //   // configure Swiper to use modules
   //   modules: [Navigation, Pagination],
@@ -32,25 +35,44 @@ export class CarouselImagesComponent implements AfterViewInit {
 
 
 
-
   ngAfterViewInit(): void {
+
+
     const element = this.swiperDiv().nativeElement;
     if (!element) return;
+    if (this.platformIdService.isBrowser()) {
+      const swiper = new Swiper(element, {
+        direction: 'horizontal',
+        loop: true,
+        modules: [Navigation, Pagination],
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+        breakpoints: {
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 1,
+            spaceBetween: 40,
+          },
+        }
+      });
+    }
 
-    const swiper = new Swiper(element, {
-      direction: 'horizontal',
-      loop: true,
-      modules: [Navigation, Pagination],
-      pagination: {
-        el: ".swiper-pagination"
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    });
+
   }
 }
